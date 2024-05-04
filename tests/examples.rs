ae48169eb9ff3c242f6fa2bfd9cfdb660add6a98
Christoph Girstenbrei <Christoph.Girstenbrei@gmail.com>
2024-05-04 15:56:35 +0200

    Test both print examples are compatible

---
+++ tests/examples.rs
@@ -0,0 +1,30 @@
+#[test]
+fn client_v_server() {
+    println!("Building and spawn the server");
+    let server = escargot::CargoBuild::new()
+        .current_release()
+        .current_target()
+        .manifest_path("./server/Cargo.toml")
+        .example("print")
+        .run()
+        .expect("Failed building client");
+    let mut server = server.command().spawn().expect("Failed running print example");
+
+    println!("Build and start the client");
+    let client = escargot::CargoBuild::new()
+        .current_release()
+        .current_target()
+        .manifest_path("./client/Cargo.toml")
+        .example("print")
+        .run()
+        .expect("Failed building client");
+
+    let exit_status = client.command().status().expect("Failed running print example");
+
+    // shutdown server
+    server.kill().expect("Failed killing server process in test");
+
+    if !exit_status.success() {
+        panic!("Client failed with status {}", exit_status);
+    }
+}
